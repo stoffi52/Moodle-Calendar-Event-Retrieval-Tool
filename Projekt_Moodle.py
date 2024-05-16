06b57f9e09cf5c0624d5d519d5b8c5a138d0a82d
dkalkus <dkalkus@gmail.com>
2024-05-16 09:08:25 +0200

    Add files via upload

---
+++ Projekt_Moodle.py
@@ -0,0 +1,32 @@
+import requests
+import json
+ 
+# Konfiguration
+moodle_url = 'https://your-moodle-site.com'
+webservice_url = moodle_url + '/webservice/rest/server.php'
+token = 'token'
+userid = 'userid'
+ 
+# API-Endpunkt und Parameter für den Kalenderabruf
+calendar_function = 'core_calendar_get_calendar_events'
+params = {
+    'wstoken': token,
+    'wsfunction': calendar_function,
+    'userid': userid
+}
+ 
+try:
+    # Anfrage an die Moodle-API senden
+    response = requests.post(webservice_url, params=params)
+    # JSON-Daten erhalten und parsen
+    data = response.json()
+    # Kalenderdaten anzeigen
+    for event in data['events']:
+        print("Event: {}".format(event['name']))
+        print("Start: {}".format(event['timestart']))
+        print("End: {}".format(event['timeend']))
+        print("Description: {}".format(event['description']))
+        print("Location: {}".format(event['location']))
+        print("----------")
+except requests.exceptions.RequestException as e:
+    print('Error:', e)
