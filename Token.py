21d43a2f2aa7971bed312693a1362a550de6b1ba
dkalkus <dkalkus@gmail.com>
2024-05-22 21:53:18 +0200

    Updated gitignore

--- Token.py
+++ Token.py
@@ -4,12 +4,12 @@ from json import dumps
 api = MoodleAPI("PW.py")
 api.login()
 site_info = api.get_site_info()
-print(site_info)
+# print(site_info)
 with open("output.json", "w") as op:
     op.write(dumps(site_info, indent=4))
 
 response = api.post("core_enrol_get_users_courses", site_info["userid"])
-print(response, type(response))
+# print(response, type(response))
 
 with open("response.json", "w") as op:
     op.write(dumps(response, indent=4))
